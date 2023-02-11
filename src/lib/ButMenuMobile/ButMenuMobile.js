57c247fb72ad10531ea48f0d8ac03393da425999
AlekseySteblivets <alsteua@gmail.com>
2023-02-11 23:48:41 +0200

    changed color for Hero and Nav

--- src/lib/ButMenuMobile/ButMenuMobile.js
+++ src/lib/ButMenuMobile/ButMenuMobile.js
@@ -1,12 +1,8 @@
-// import React, { useState } from 'react';
-
 import cn from 'classnames';
 
 import styles from './ButMenuMobile.module.scss';
 
 export default function ButMenuMobile({ isOpenMenuMobile }) {
-  // const [mobileMenu, setMobileMenu] = useState(false);
-
   const onClick = () => {
     console.log('onClick');
     isOpenMenuMobile();
@@ -24,33 +20,3 @@ export default function ButMenuMobile({ isOpenMenuMobile }) {
     </div>
   );
 }
-
-// export default function Button({
-//   onClick = () => {},
-//   href,
-//   children,
-//   disabled,
-//   type = "button",
-//   className,
-// }) {
-//   const Tag = ({ ...props }) =>
-//     href ? <a {...props} /> : <button {...props} />;
-
-//   return (
-//     <Tag
-//       href={href}
-//       onClick={onClick}
-//       type={type}
-//       className={cn(
-//         styles.button,
-//         {
-//           [styles.buttonDisabled]: disabled,
-//         },
-//         className
-//       )}
-//       disabled={disabled}
-//     >
-//       {children}
-//     </Tag>
-//   );
-// }
